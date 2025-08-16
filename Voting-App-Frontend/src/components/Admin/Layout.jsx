98e388c949e6b7f67dff9e6ec3db8e17a1b73238
Raj Ugavekar <ugavekarraj@gmail.com>
2025-08-16 21:29:14 +0530

    Bug Fixes

--- Voting-App-Frontend/src/components/Admin/Layout.jsx
+++ Voting-App-Frontend/src/components/Admin/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Header from './Header';
@@ -22,9 +22,21 @@ export default function Layout() {
       })
   }, [dispatch]);
 
+  	const menuRef = useRef(null);
+
+	  useEffect(() => {
+		function handleClickOutside(e) {
+		  if (menuRef.current && !menuRef.current.contains(e.target)) {
+			setSidebarOpen(!sidebarOpen);
+		  }
+		}
+		document.addEventListener("mousedown", handleClickOutside);
+		  return () => document.removeEventListener("mousedown", handleClickOutside);
+	  }, []);
+
 	return (
 		<div className="bg-neutral-100 h-screen w-screen overflow-hidden flex">
-			<div className={`fixed inset-y-0 left-0 transform lg:relative lg:translate-x-0 transition-transform duration-300 ease-in-out ${sidebarOpen ? "translate-x-0" : "-translate-x-full"} lg:w-60 w-64 bg-gray-100 z-50`}>
+			<div className={`fixed inset-y-0 left-0 transform lg:relative lg:translate-x-0 transition-transform duration-300 ease-in-out ${sidebarOpen ? "translate-x-0" : "-translate-x-full"} lg:w-60 w-64 bg-gray-100 z-50`} ref={menuRef}>
 				<Sidebar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
 			</div>
 			<div className="flex flex-col flex-1">
